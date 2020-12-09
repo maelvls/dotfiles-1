65ba5f6b53b67d1ad16835d1b2f20a009d131821
Claudia Beresford <claudiaberesford@gmail.com>
2020-12-09 17:21:03 +0100

    okok

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ install_brew() {
 }
 
 install_ansible() {
-  if ! command -v ansible-playbook > /dev/null 2>&1 ; then
+  if ! command -v ansible-playbook >/dev/null 2>&1; then
     log "ansible-playbook not found on \$PATH, installing"
     install_brew "$1"
     brew install ansible
@@ -25,19 +25,16 @@ install_ansible() {
 }
 
 install_ansible_linux() {
-	sudo apt-get update
-	sudo apt-get install -y python python3.7 libssl-dev
-	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-	python get-pip.py --user
-	pip install --user ansible
-	rm get-pip.py
+  sudo apt-get install -y python3 python3-pip libssl-dev
+  pip3 install --user ansible
 }
 
 run_it() {
   log "Configuring machine for $tag"
   (
     cd "$(dirname "$0")"
-    cmd="ansible-playbook -i localhost, --tags $tag --con local playbook.yml"
+    ansible-galaxy install -r requirements.yml
+    cmd="ansible-playbook -v -e ansible_python_interpreter=/usr/bin/python3 -i localhost, --tags $tag --con local playbook.yml"
     $cmd
   )
 
@@ -48,23 +45,22 @@ run_it() {
 
 user="$*"
 case $user in
-  m)
-    tag="m-rcd"
-    install_ansible
-    ;;
-  c)
-    tag="callisto"
-    install_ansible "$tag"
-    ;;
-  l)
-    tag="linux"
-    install_ansible_linux
-    ;;
-  *)
-    log "Usage: ./run.sh <m|c|l>"
-    exit 0
-    ;;
+m)
+  tag="m-rcd"
+  install_ansible
+  ;;
+c)
+  tag="callisto"
+  install_ansible "$tag"
+  ;;
+l)
+  tag="linux"
+  install_ansible_linux
+  ;;
+*)
+  log "Usage: ./run.sh <m|c|l>"
+  exit 0
+  ;;
 esac
 
 run_it
-
